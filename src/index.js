586c49a48c99cd8215298947c84821fdf800c05f
alvics <alvics71@hotmail.com>
2018-12-19 19:40:16 +1000

    blank canvas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
+// class component
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  // Definie & Intializing 'state'
+  constructor(props) {
+    // when using the constructor, it overrides the built in React.Component constructor
+    super(props); // super is a referance to the parent React.Component
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    this.state = { lat: null }; // Inializing 'state' direct assignment, JS object (key value pairs) with it's default value to null
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+          // updating the state, rerendering
+          this.setState({ lat: position.coords.latitude }) // JS object
+      },
+      err => console.log(err)
+    );
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+// Understanding 'state'
+
+/* Only usable with class components,
+   You will confuse props with this.state,
+   'state' is a JS object that contains data relevant to a component,
+   Updating 'state' on a component causes the component to (almost) instantly rerender,
+   'state' must be initialized when a component is created (constructor), 
+   'state' can ONLY be updated using the function 'setState'
+   */
+
+/* JS constructor() 
+      is the very first function that is called in the instance that it's created
+   */
